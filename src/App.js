1657f001cf4422e5428d4a3cc763e5e695489239
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-08-03 17:07:40 +0530

    loading feature is added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const fetchMovieHandler = async () => {
+    setIsLoading(true);
+
     const response = await fetch("https://swapi.py4e.com/api/films/");
     const data = await response.json();
-    const transformedData = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedData);
+    setTimeout(() => {
+      const transformedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setIsLoading(false);
+      setMovies(transformedData);
+    }, 1000);
   };
 
   return (
@@ -25,7 +31,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {isLoading && <h1>Loading....</h1>}
+        {!isLoading && <MoviesList movies={movies} />}
+        {!isLoading && <h2>No Movies Found</h2>}
       </section>
     </React.Fragment>
   );
